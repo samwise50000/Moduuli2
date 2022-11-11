bd84fa76e591e1746db219a2637b5d2c792e735a
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-11 19:17:33 +0200

    Tehtäviä

--- Moduuli2_teht5/moduuli2_teht5.js
+++ Moduuli2_teht5/moduuli2_teht5.js
@@ -1,1 +1,15 @@
 'use script';
+let target = document.querySelector('#target')
+let number_list = []
+
+for(let i = 0; true ; i++){
+    let number = parseInt(prompt('Give a number you havent used'))
+    if (number_list.includes(number)){
+        break
+    }else{
+        number_list[i] = number
+    }
+}
+target.innerHTML = ('Number is already given')
+let sorted_list = number_list.sort(function (a, b){return a-b});
+console.log(sorted_list)
