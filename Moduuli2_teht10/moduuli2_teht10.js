696872326a35f5924fcb16e68385c409ffb9eeab
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-11 14:56:56 +0200

    Tehtäviä

--- Moduuli2_teht10/moduuli2_teht10.js
+++ Moduuli2_teht10/moduuli2_teht10.js
@@ -1,1 +1,20 @@
 'use script';
+
+let nof_candi = parseInt(prompt('Number of candidates?'));
+let candidates_list = [];
+
+for (let i = 0; i < nof_candi; i++) {
+
+    let cand_name = prompt('Name for candidate ' + (i + 1));
+
+    let candidate =
+        {
+        name: cand_name,
+        votes: 0
+
+
+    }
+    candidates_list.push(candidate);
+}
+
+console.log(candidates_list)
