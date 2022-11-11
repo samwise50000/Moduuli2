c11df413a57de30a94abd111dc903585b28f5e07
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-11 19:12:43 +0200

    Tehtäviä

--- Moduuli2_teht3/moduuli2_teht3.js
+++ Moduuli2_teht3/moduuli2_teht3.js
@@ -9,11 +9,11 @@ for(let i = 0; i < dogs_amount; i++){
 let sorted_dogs = dogs.sort()
 let r_sorted_dogs = sorted_dogs.reverse()
 // Generoidaan html-lista koirien nimistä
-let html = '<ol>';
+let html = '<ul>';
 for(let i = 0; i < 6; i++){
     html += '<li>';
     html += r_sorted_dogs[i];
     html += '</li>';
 }
-html += '</ol>';
+html += '</ul>';
 target.innerHTML = html
--- Moduuli2_teht6/moduuli2_teht6.js
+++ Moduuli2_teht6/moduuli2_teht6.js
@@ -1,1 +1,23 @@
 'use script';
+
+let target = document.querySelector('#target')
+
+function throwDice() {
+    let diceNumber = Math.floor(Math.random() * 6) + 1;
+    return diceNumber
+}
+
+let output_list = '<ul>';
+while (true) {
+    let numberDice = throwDice()
+    output_list += '<li>';
+    output_list += numberDice;
+    output_list += '</li>';
+    console.log(numberDice)
+    if (numberDice === 6) {
+        break;
+    }
+}
+output_list += '</ul>';
+target.innerHTML = output_list
+
