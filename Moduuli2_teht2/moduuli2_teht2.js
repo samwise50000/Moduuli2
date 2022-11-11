822c201e1b7a6a7c2107c41b080825bdf9a1d783
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-11 14:39:27 +0200

    Tehtäviä

--- Moduuli2_teht2/moduuli2_teht2.js
+++ Moduuli2_teht2/moduuli2_teht2.js
@@ -1,1 +1,26 @@
 'use script';
+let target = document.querySelector('#target');
+
+// Kysytään osanottajien määrä
+let nof_p = parseInt(prompt('How many participants?'));
+let participants = [];
+
+// Luetaan osanottajien nimet
+for (let i = 0; i < nof_p; i++)
+{
+participants[i] = prompt('Name: ');
+}
+
+// Generoidaan html-lista osanottajien nimistä
+let html_lista = '<ol>';
+for (let i = 0; i < nof_p; i++)
+{
+    html_lista += '<li>';
+    html_lista += participants[i];
+    html_lista += '</li>';
+}
+
+html_lista += '</ol>';
+
+// Lisätään generoitu html sivulle
+target.innerHTML = html_lista
